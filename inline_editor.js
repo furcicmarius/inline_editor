f67f36a064488b02a680a6c910da5027f6fbf361
Marius Furcic <mariusfurcic@gmail.com>
2013-05-30 19:49:07 +0300

    Update module

--- inline_editor.js
+++ inline_editor.js
@@ -3,6 +3,7 @@
 
   Drupal.ajax.prototype.commands.removeClickDisableClass = function(ajax, response, status) {
     $('.custom-editable').removeClass('click-disabled');
+    $('.inline-edit-button').fadeIn(500);
   }
 
   Drupal.ajax.prototype.getForm = function(fieldName, entityType, id) {
@@ -59,8 +60,8 @@
           });
 
           if ($(this).closest('.field-collection-container').length) {
+
             // For field collection add a single class
-            // console.log($(this));
             $(this).closest('.field-collection-container').addClass('custom-editable ' + fieldName);
           }
           else if (!$(this).find('.field-items').hasClass('inline-editor-cke')) {
@@ -74,69 +75,127 @@
 
   $(document).ready(function() {
 
+    var editButton = '<div class="inline-edit-button">Edit</div>',
+        inlineTargetElement = 1;
+
     if (typeof Drupal.settings.ckeFields != 'undefined') {
-      $.each(Drupal.settings.ckeFields, function(field,type){
+      if (Drupal.settings.ckeditorExists) {
 
-        var fieldName = field.replace(/_/g, '-');
+        $.each(Drupal.settings.ckeFields, function(field,type){
 
-        $('.field-name-' + fieldName + ' .field-items').attr('id', 'cke-edit-id-' + fieldName);
-        $('#cke-edit-id-' + fieldName).addClass('inline-editor-cke')
-                                      .attr('contenteditable', 'true')
-                                      .attr({'cke-data-id' : Drupal.settings.id, 'cke-data-fieldname' : field, 'cke-data-entity-type' : Drupal.settings.entityType, 'cke-data-field-type' : type})
-                                      .parent().removeClass('custom-editable');
-      });
+          var fieldName = field.replace(/_/g, '-');
 
-      // Make title editable
-      if($('#page-title').length) {
-        $('#page-title').addClass('inline-editor-cke')
-                        .attr('contenteditable', 'true')
-                        .attr({'cke-data-id' : Drupal.settings.id, 'cke-data-fieldname' : 'title', 'cke-data-entity-type' : Drupal.settings.entityType, 'cke-data-field-type' : 'text'});
+          $('.field-name-' + fieldName + ' .field-items').attr('id', 'cke-edit-id-' + fieldName);
+          $('#cke-edit-id-' + fieldName).addClass('inline-editor-cke')
+                                        .attr({
+                                          'contenteditable': 'true',
+                                          'cke-data-id' : Drupal.settings.id,
+                                          'cke-data-fieldname' : field,
+                                          'cke-data-entity-type' : Drupal.settings.entityType,
+                                          'cke-data-field-type' : type.field,
+                                          'cke-data-text-preocessing' : type.text_processing
+                                        }).parent().removeClass('custom-editable');
+        });
+        // Make title editable
+        if($('#page-title').length) {
+          $('#page-title').addClass('inline-editor-cke')
+                          .attr({
+                            'contenteditable': 'true',
+                            'cke-data-id' : Drupal.settings.id,
+                            'cke-data-fieldname' : 'title',
+                            'cke-data-entity-type' : Drupal.settings.entityType,
+                            'cke-data-field-type' : 'text',
+                            'cke-data-text-preocessing' : 0
+                          })
+                          .addClass('inline-target-' + inlineTargetElement)
+                          .before($(editButton).attr('target-element', 'inline-target-' + inlineTargetElement));
+
+        }
       }
     }
 
-    $(".inline-editor-cke").each(function( index ) {
+    $("body").delegate(".inline-edit-button", "click", function(){
+      var targetElem = $(this).attr('target-element');
+      $('.' + targetElem).click().focus();
+      $('.inline-edit-button').fadeOut(500);
+    });
+
+    $("body").delegate(".form-autocomplete", "keydown", function(e) {
+      if (e.which == 13 && $('#autocomplete').length > 0) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+      }
+    });
+
+    // Add edit button
+    $('.custom-editable').each(function(){
+      inlineTargetElement++;
+      $(this).addClass('inline-target-' + inlineTargetElement)
+             .before($(editButton).attr('target-element', 'inline-target-' + inlineTargetElement));
+    });
+
+    $('.inline-editor-cke').each(function( index ) {
+
+      // Add edit button
+      inlineTargetElement++;
+      $(this).addClass('inline-target-' + inlineTargetElement)
+             .parent('.field').before($(editButton).attr('target-element', 'inline-target-' + inlineTargetElement));
+
       if (typeof CKEDITOR != 'undefined') {
         var content_id = $(this).attr('id'),
             tpl = $(this).attr('tpl'),
-            prevValue = $(this)[0]['innerText'];
+            // nu le mai vreau
+            innerHTML = $(document.getElementById(content_id).innerHTML).html() ?
+                          $(document.getElementById(content_id).innerHTML).html() :
+                          document.getElementById(content_id).innerHTML,
+            prevValue = innerHTML.trim();
 
+            console.log($('#' + content_id).html());
+
+        //console.log($(this)..html());
+        // console.log($(this).html($($(this).html())));
+        //$.data()
         $(this).attr('cke-data-prev-value', prevValue);
 
-        var that = this;
+        textProcessing = $(this).attr('cke-data-text-preocessing');
 
         CKEDITOR.inline( content_id, {
           on: {
             blur: function( event ) {
+              $('.inline-edit-button').fadeIn(500);
+              var output = {},
+                  fieldElement = $('#' + content_id),
+                  textProcessing = fieldElement.attr('cke-data-text-preocessing') == 1 ? true : false;
+                  prevValue = fieldElement.attr('cke-data-prev-value').trim();
 
-              var output = {};
+              output['data'] = event.editor.getData().trim();
 
-              // Get input value
-              output['data'] = event.editor.getData();
+              if (!textProcessing) {
 
-              // Get only text without tags
-              // Appending <p> prevents data loss
-              output['data'] = $('<p>'  + output['data'] + '</p>').text();
+                // Get only text without tags
+                output['data'] = strip_tags(output['data']);
+                prevValue = strip_tags(prevValue);
 
+              }
 
-              if (output['data'].trim() != $(that).attr('cke-data-prev-value').trim()) {
+              if (output['data'] != prevValue) {
+                fieldElement.attr('cke-data-prev-value', output['data'].trim());
 
-                $(that).attr('cke-data-prev-value', output['data'].trim());
-
-                output['fieldName'] = $(that).attr('cke-data-fieldname'),
-                output['id'] = $(that).attr('cke-data-id'),
-                output['entityType'] = $(that).attr('cke-data-entity-type');
-                output['fieldType'] = $(that).attr('cke-data-field-type');
+                output['fieldName'] = fieldElement.attr('cke-data-fieldname'),
+                output['id'] = fieldElement.attr('cke-data-id'),
+                output['entityType'] = fieldElement.attr('cke-data-entity-type');
+                output['fieldType'] = fieldElement.attr('cke-data-field-type');
 
                 $.ajax({
                   url: Drupal.settings.basePath + 'save-cke-inline-data',
                   dataType: 'json',
                   type: "POST",
-                  async: true,
                   data: {
                       data : output,
                   },
                   success: function(data) {
-                    console.log(data);
+                    console.log('data: ' + data);
                   },
                   error: function(data) {
                     console.log(data);
@@ -144,23 +203,33 @@
                 });
               }
 
+            },
+            focus : function(event) {
+              $('.inline-edit-button').fadeOut(500);
             }
           },
 
-          toolbar :[
-            ['Source'],
-            ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
-            ['Undo','Redo','Find','Replace','-','SelectAll','RemoveFormat'],
-            [/*'Image',*/'Flash','Table','HorizontalRule','Smiley','SpecialChar'],
-            ['Maximize', 'ShowBlocks'],
-            '/',
-            ['Format'],
-            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-            ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiRtl','BidiLtr'],
-            ['Link','Unlink','Anchor','Linkit','LinkToNode','LinkToMenu'],
-            ['DrupalBreak', 'DrupalPageBreak']
-          ],
+          toolbar : (
+            textProcessing == 1 ?
+              [
+                ['Source', 'Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
+                ['Undo','Redo','Find','Replace','-','SelectAll','RemoveFormat'],
+                ['Flash','Table','HorizontalRule','Smiley','SpecialChar'],
+                ['Maximize', 'ShowBlocks'],
+                '/',
+                ['Format'],
+                ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+                ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+                ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiRtl','BidiLtr'],
+                ['Link','Unlink','Anchor','Linkit','LinkToNode','LinkToMenu'],
+                ['DrupalBreak', 'DrupalPageBreak']
+              ]
+              :
+              [
+                ['Source', 'Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker', 'Scayt'],
+                ['Undo','Redo','Find','Replace','-','SelectAll'],
+              ]
+          ),
         });
       }
     });
@@ -206,6 +275,8 @@
         return false;
       }
 
+      $('.inline-edit-button').fadeOut(500);
+
       var fieldName,
           classes = $(this).attr('class').split(' '),
           thisClicked = this;
@@ -222,8 +293,10 @@
 
       var html_container = thisClicked.outerHTML;
 
-      $(thisClicked).removeClass('custom-editable').addClass('parent custom-edit-in-use current-edit custom-editable-' + fieldName).attr('data', html_container);
-      // $(thisClicked).remove();
+      $(thisClicked).removeClass('custom-editable')
+                    .addClass('parent custom-edit-in-use current-edit custom-editable-' + fieldName)
+                    .attr('data', html_container);
+
       $('.custom-editable').addClass('click-disabled');
 
       Drupal.ajax['custom_ajax_action'].getForm(fieldName, entityType, id);
@@ -262,6 +335,7 @@
 
       $($parent).replaceWith(data);
       $('.custom-editable').removeClass('click-disabled');
+      $('.inline-edit-button').fadeIn(500);
 
     });
 
@@ -286,5 +360,14 @@
   //     var $ = Aloha.jQuery;
   //     $('.custom-edit').aloha();
   // });
+
+function strip_tags (input, allowed) {
+  allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+  var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+    commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+  return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+    return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+  });
+}
 
 })(jQuery);
